457fa77b57d526779d41c197579d1f3005605719
Wiktoria Dębowska <104149895+IdKheh@users.noreply.github.com>
2023-12-19 00:49:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, send_from_directory
 import serial
 import os
 import sqlite3
 from PIL import Image
 from PIL import ImageDraw
+from werkzeug.utils import secure_filename
 
-# ser = serial.Serial('/dev/ttyACM0',9600, timeout =1) //polaczenie ardiuno i raspberry
+ser = serial.Serial('/dev/ttyACM0',9600, timeout =1) #polaczenie ardiuno i raspberry
 serGPS = serial.Serial('/dev/ttyUSB0',9600) # polaczenie raspberry i GPS
 conn = sqlite3.connect('GPSData.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -13,12 +14,11 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS DATA(id INTEGER PRIMARY KEY, x INTE
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-## dodaj statyczny adres IP: https://www.instructables.com/Raspberry-Pi-GPS-Tracker/
-## jakiś tutorial https://www.youtube.com/watch?v=4R5m7xsU-0g
-
 @app.route('/')
 def index():
-    action(0)
+    ser.write(str(0).encode('utf-8'))
+    ser.write(str(9).encode('utf-8'))
+    
     return render_template('index.html')
 
 
@@ -28,23 +28,33 @@ def execCommand(FUNCTION = None):
     return ""
 
 
-def draw_and_return(coordinates):
-    img = Image.new("RGB", (1000, 1000), color = (255, 255, 255))
+def draw_and_return(coordinates,number):
+    print(coordinates)
+    img = Image.new("RGB", (500, 500), color = (255, 255, 255))
     img1 = ImageDraw.Draw(img)
-    positionX = coordinates[0][0]
-    positionY = coordinates[0][1]
-    for i in range(1,len(coordinates)-1):
+    
+    try:
+        positionX = coordinates[0][0]
+        positionY = coordinates[0][1]
+                
+        for i in range(1,len(coordinates)-1):
 
-        start = (positionX - coordinates[i][0], positionY - coordinates[i][1])
-        end = (positionX - coordinates[i+1][0], positionY - coordinates[i+1][1])
-        img1.line((start, end),fill=0, width=2)
+            start = (abs(coordinates[i][0]-positionX)/50, abs(coordinates[i][1]-positionY)/50)
+            end = (abs(coordinates[i+1][0]-positionX)/50, abs(coordinates[i+1][1]-positionY)/50)
+            print(start)
+            print(end)
+            img1.line((start, end),fill=5, width=5)
+    except:
+        print('ERROR: IMG')
+        start = (0,0)
+        img1.line((start, start),fill=5, width=2)
 
-    img.save(f"static/img.png")
+    print(number)
+    img.save(f"static/img"+str(number)+".png")
 
 
 def action(direction : int):
-    # ser.write(str(direction).encode('utf-8'))
-
+    ser.write(str(direction).encode('utf-8'))
     makeData()
     return ''
 
@@ -55,6 +65,7 @@ def delete():
 
 def insert(x: float, y: float):
     cursor.execute("INSERT INTO DATA (x, y) VALUES(?,?)", (x, y))
+    conn.commit()
 
 
 def select(l: list):
@@ -69,48 +80,58 @@ def select(l: list):
 
 
 def makeData():
-    while(True):
-        nmea_sentence = serGPS.readline()
-        message = nmea_sentence.decode("utf-8").replace("\n", "").replace("\r", "").split(',')
-        if message[0] == '$GPGGA':
-            if (message[2] != '' and message[4] != ''):
-                x = int(message[2].replace(".", ""))
-                y = int(message[4].replace(".", ""))
-                insert(x, y)
-                break
+    errors = 0
+    while(errors<20):
+        try:
+            nmea_sentence = serGPS.readline()
+            message = nmea_sentence.decode("utf-8").replace("\n", "").replace("\r", "").split(',')
+            if message[0] == '$GPGGA':
+                if (message[2] != '' and message[4] != ''):
+                    x = int(message[2].replace(".", ""))
+                    y = int(message[4].replace(".", ""))
+                    print(x,y)
+                    insert(x,y)
+                    errors = 100
+                    break
+        except:
+            errors +=1
+            print('ERROR: GPS')
+            pass
+
+def deleteFile(number):
+    for i in range(1,number+1):
+        os.remove('static/img'+str(i)+'.png')
 
 
-def deleteFile():
-    os.remove("static/img.png")
-
-
-def draw():
+def draw(number : int):
+    print("number: "+str(number))
+    counter= number
     action(8)  # blokada ruchu robota
     l = []
     select(l)
-    draw_and_return(l)
+    draw_and_return(l,number)
     action(9)
-    return ''
+    return ""
+    
 
-def reset():
+def reset(number : int):
     delete()
     conn.commit()
     cursor.execute('''CREATE TABLE IF NOT EXISTS DATA(id INTEGER PRIMARY KEY, x INTEGER, y INTEGER);''')
     conn.commit()
-    deleteFile()
+    #deleteFile(number)
     return ''
 
 
-def poweroff():
+def poweroff(number : int):
     delete()
     conn.commit()
     cursor.close()
-    deleteFile()
-    # os.system('shutdown')
+    deleteFile(number)
+    os.system('shutdown -h now')
     return ''
 
 
 if __name__ == '__main__':
-    app.run()
-    # app.run(debug=True, port=80, host='0.0.0.0')
-    # ser.reset_input_buffer()
+    app.run(debug=False, host='0.0.0.0')
+    ser.reset_input_buffer()
