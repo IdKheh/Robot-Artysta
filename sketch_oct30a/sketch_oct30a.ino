7b72d0831d45f24fa3d07ba6e1bd4aa26472355d
Wiktoria Dębowska <104149895+IdKheh@users.noreply.github.com>
2023-12-19 00:53:25 +0100

    Update sketch_oct30a.ino

--- sketch_oct30a/sketch_oct30a.ino
+++ sketch_oct30a/sketch_oct30a.ino
@@ -1,33 +1,58 @@
 void setup() {
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(9,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  digitalWrite(3,HIGH); //PWM jednego silnika
+  
+  pinMode(8,OUTPUT); 
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(6,HIGH); //PWM drugiego silnika
   Serial.begin(9600);
 }
-
+int blokada = 0;
 void loop() {
   if(Serial.available()>0){
     int kierunek = Serial.read()-'0';
-    switch(kierunek){
-      case 1:
-        digitalWrite(12,HIGH);
-        break;
-      case 2:
-        digitalWrite(11,HIGH);
-        break;
-      case 3:
-        digitalWrite(10,HIGH);
-        break;
-      case 4:
-        digitalWrite(9,HIGH);
-        break;
-      default:
-        digitalWrite(12,LOW);
-        digitalWrite(11,LOW);
-        digitalWrite(10,LOW);
-        digitalWrite(9,LOW);
-        break;
-      }   
-    }
+    if(kierunek == 8) blokada = 1;
+    if(kierunek == 9) blokada = 0;
+    if(!blokada){
+      switch(kierunek){
+        case 1: //jazda do przodu
+          digitalWrite(8,HIGH);
+          digitalWrite(2,HIGH);
+          digitalWrite(4,LOW);
+          digitalWrite(7,LOW);       
+          break;
+          
+        case 2: //jazda do lewo
+          digitalWrite(8,HIGH);
+          digitalWrite(4,HIGH);
+          digitalWrite(2,LOW);
+          digitalWrite(7,LOW);
+          break;
+          
+        case 3: //jazda do prawo
+          digitalWrite(7,HIGH);
+          digitalWrite(2,HIGH);
+          digitalWrite(4,LOW);
+          digitalWrite(8,LOW);
+          break;
+          
+        case 4: //jazda do tyłu
+          digitalWrite(7,HIGH);
+          digitalWrite(4,HIGH);
+          digitalWrite(2,LOW);
+          digitalWrite(8,LOW);
+          break;
+          
+        default:
+          digitalWrite(2,LOW);
+          digitalWrite(4,LOW);
+          digitalWrite(7,LOW);
+          digitalWrite(8,LOW);
+          break;
+        }   
+      }
+    } 
 }
